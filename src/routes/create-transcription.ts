d167def5ecca1f268292b01c6bd4c1067c06e9f8
Daniel Lucas dos Santos Borges <dlsbdaniel@gmail.com>
2023-09-14 10:58:04 -0300

    Fix formatting

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -5,48 +5,48 @@ import { createReadStream } from "node:fs";
 import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-    app.post('/videos/:videoId/transcription', async (request) => {
-        const paramsSchema = z.object({
-            videoId: z.string().uuid(),
-        });
+  app.post('/videos/:videoId/transcription', async (request) => {
+    const paramsSchema = z.object({
+      videoId: z.string().uuid(),
+    });
 
-        const { videoId } = paramsSchema.parse(request.params);
+    const { videoId } = paramsSchema.parse(request.params);
 
-        const bodySchema = z.object({
-            prompt: z.string()
-        });
+    const bodySchema = z.object({
+      prompt: z.string()
+    });
 
-        const { prompt } = bodySchema.parse(request.body);
+    const { prompt } = bodySchema.parse(request.body);
 
-        const video = await prisma.video.findUniqueOrThrow({
-            where: {
-                id: videoId,
-            }
-        });
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    });
 
-        const videoPath = video.path;
-        const audioReadStream = createReadStream(videoPath);
+    const videoPath = video.path;
+    const audioReadStream = createReadStream(videoPath);
 
-        const response = await openai.audio.transcriptions.create({
-            file: audioReadStream,
-            model: 'whisper-1',
-            language: 'pt',
-            response_format: 'json',
-            temperature: 0,
-            prompt,
-        });
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
+    });
 
-        const transcription = response.text;
+    const transcription = response.text;
 
-        await prisma.video.update({
-            where: {
-                id: videoId,
-            },
-            data: {
-                transcription
-            }
-        });
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription
+      }
+    });
 
-        return { transcription };
-    }); 
+    return { transcription };
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 const app = fastify();
 
 app.register(fastifyCors, {
-    origin: '*',
+  origin: '*',
 });
 
 app.register(getAllPromptsRoute);
@@ -17,7 +17,7 @@ app.register(createTranscriptionRoute);
 app.register(generateAICompletionRoute);
 
 app.listen({
-    port: 3333,
+  port: 3333,
 }).then(() => {
-    console.log("HTTP Server Running!");
+  console.log("HTTP Server Running!");
 });
